04b896cfd3192d6c665b7aa1f2d002f02acc7706
kwizeraherve70 <kwizeraherve70@gmail.com>
2026-04-06 01:30:03 +0200

    config frontend url to cors

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(
 app.use(json());
 app.use(
   cors({
-    origin: ["https://kigalihotmarket-frontend.vercel.app"],
+    origin: ["https://kigalihotmarket-frontend.vercel.app", "https://kigalihotmarket-fontend-hs54pegw0.vercel.app"],
     credentials: true,
   }),
 );
